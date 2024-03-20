eee7eb6cd1b04c3d2eb707007d499d21aa66f8ce
Shsin9797 <aaa8141@naver.com>
2024-03-20 22:42:15 +0900

    feat:  토큰을 만들어서 만료날자를 설정하고, memberId를 클레임으로 추가하고, 설정된 알고리즘으로 토큰을 서명하고 반환하는 메서드 구현

--- src/main/java/backend/likelion/todos/auth/jwt/JwtService.java
+++ src/main/java/backend/likelion/todos/auth/jwt/JwtService.java
@@ -5,6 +5,7 @@ import static java.util.concurrent.TimeUnit.MILLISECONDS;
 
 import backend.likelion.todos.common.UnAuthorizedException;
 import com.auth0.jwt.JWT;
+import com.auth0.jwt.JWTCreator;
 import com.auth0.jwt.algorithms.Algorithm;
 import com.auth0.jwt.exceptions.JWTVerificationException;
 import java.util.Date;
@@ -25,9 +26,13 @@ public class JwtService {
     // 회원 ID를 기반으로 JWT 토큰을 생성합니다.
     public String createToken(Long memberId) {
         // TODO [5단계] 현재 시간과 설정된 만료 시간을 사용하여 만료 날짜를 설정하세요.
+        JWTCreator.Builder expireDate = JWT.create().withExpiresAt(new Date(accessTokenExpirationDayToMills + System.currentTimeMillis())).withIssuedAt(new Date());
         // TODO [5단계] memberId를 클레임으로 추가하세요.
+        JWTCreator.Builder expireDateAddedMemberId = expireDate.withClaim("memberId",memberId);
         // TODO [5단계] 설정된 알고리즘으로 토큰을 서명하고 반환하세요.
-        return null;
+        String tokenWithAlgorithmSign = expireDateAddedMemberId.sign(algorithm);
+
+        return tokenWithAlgorithmSign;
     }
 
     // 토큰에서 회원 ID를 추출합니다.
